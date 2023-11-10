2c23207a7bdb67b2e270b35ad21e95fd0365263f
felipegilisk <felipegilisk@gmail.com>
2023-11-09 21:34:32 -0300

    adicionados parâmetros customizados no método GET da API

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,9 @@ import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoListData;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +28,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<MedicoListData> listarMedicos() {
-        return repository.findAll().stream().map(MedicoListData::new).toList();
+    public Page<MedicoListData> listarMedicos(@PageableDefault(size = 10, sort = {"especialidade"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(MedicoListData::new);
     }
 }
